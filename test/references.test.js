f432b67dc16cdf873f7d482713a4aeb95061420a
Paul Smith <paul@paulsmith.io>
2020-05-28 09:38:51 +0200

    references test

--- test/references.test.js
+++ test/references.test.js
@@ -1,20 +1,41 @@
 import { Converter } from '../dist'
-import vehicles from './fixtures/vehicles'
+import luke from './fixtures/luke'
 
-describe('references', done => {
-  test('basic referencing', () => {
-    let shipReferencer = mappingObj => {}
+describe('references', () => {
+  test('basic referencing', done => {
+    let homeworldReferencer = mappingObj => {
+      let hwId = parseInt(mappingObj.result.replace('https://swapi.co/api/planets/', ''))
+      return [
+        { id: hwId, type: 'homeworld' },
+        { id: hwId, value: mappingObj.result }
+      ]
+    }
     let template = {
       mappings: [
         { path: 'name', query: 'name', processors: ['trim'] },
         {
-          path: 'ships',
-          query: 'starships',
-          processors: [{ processor: 'map', args: ['upper'] }],
-          hook: shipHook
+          path: 'homeworld',
+          query: 'homeworld',
+          ref: homeworldReferencer
         }
       ]
     }
     let converter = new Converter(template)
+    converter
+      .render(luke)
+      .then(converted => {
+        expect(converted.references).toEqual([
+          {
+            value: 'https://swapi.co/api/planets/1/',
+            id: 1
+          }
+        ])
+        expect(converted.result.homeworld).toEqual({
+          id: 1,
+          type: 'homeworld'
+        })
+        done()
+      })
+      .catch(error => done(error))
   })
 })
